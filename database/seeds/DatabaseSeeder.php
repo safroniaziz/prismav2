bd167e74d1a7ea80020389086c8b5cbc7ddda8c3
safroniaziz <safroni.aziz@gmail.com>
2020-04-16 23:08:56 +0700

    add jadwal usulan migration

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
+        $this->call(JadwalUsulansTableSeeder::class);
         // $this->call(SkimsTableSeeder::class);
         // $this->call(BidangPenelitiansTableSeeder::class);
         // $this->call(UsulansTableSeeder::class);
