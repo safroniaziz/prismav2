b070e0167c74b1a9f2b0625811a180e15942f4d5
safroniaziz <safroni.aziz@gmail.com>
2021-04-23 14:14:55 +0700

    new commit

--- resources/views/reviewer/usulan/menunggu/index.blade.php
+++ resources/views/reviewer/usulan/menunggu/index.blade.php
@@ -115,7 +115,7 @@
                                                     <td class="text-center">
                                                         {{-- <a class="btn btn-primary btn-sm" href="{{ asset('upload/file_usulan/'.Str::slug(Session::get('nm_dosen')).'-'.Str::slug(Session::get('nip')).'/'.$usulan->file_usulan) }}" download="{{ $usulan->file_usulan }}"><i class="fa fa-download"></i></a> --}}
 
-                                                        <a class="btn btn-primary btn-sm" href="{{ asset('upload/file_usulan/'.Str::slug($usulan->nm_ketua_peneliti).'-'.$usulan->ketua_peneliti_nip.'/'.$usulan->file_usulan) }}" download="{{ $usulan->file_usulan }}"><i class="fa fa-download"></i></a>
+                                                        <a class="btn btn-primary btn-sm" href="{{ asset('upload/file_usulan/'.\Str::slug($usulan->nm_ketua_peneliti).'-'.$usulan->ketua_peneliti_nip.'/'.$usulan->file_usulan) }}" download="{{ $usulan->file_usulan }}"><i class="fa fa-download"></i></a>
                                                         {{-- <a href="{{ asset('storage/'.$usulan->file_usulan) }}" download="{{ $usulan->file_usulan }}" class="btn btn-primary btn-sm"><i class="fa fa-download"></i></a> --}}
                                                     </td>
                                                     <td style="text-align:center;">
